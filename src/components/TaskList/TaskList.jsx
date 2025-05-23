5b64d1267851ba4c31c64b52d7e3ca6405333d28
yukti-says <150407207+Yukti2004@users.noreply.github.com>
2025-05-23 18:02:41 +0530

    create Tasklist

---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+
+const TaskList = () => {
+    return (
+        <div id="TaskList" className="h-[55%] overflow-x-auto w-full   py-5  flex items-center justify-start flex-nowrap gap-5 mt-10 " >
+            <div className=" h-full p-5 w-[300px] flex-shrink-0 bg-red-400 rounded-xl   " >   
+                <div className="flex justify-between items-center" >
+                    <h3 className="bg-red-600 px-3 py-1 rounded text-sm cursor-pointer " >High</h3>
+                    <h4 className="text-sm" >20  May 2025</h4>
+                </div>
+                <h2 className="mt-5 text-2xl font-bold " >Create a cup of tea and have in the morning</h2>
+                <p className="text-sm mt-3 " >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex aperiam aliquam quam optio quibusdam architecto.</p>
+            </div>
+
+            <div className=" h-full p-5 w-[300px] flex-shrink-0 bg-red-400 rounded-xl   " >   
+                <div className="flex justify-between items-center" >
+                    <h3 className="bg-red-600 px-3 py-1 rounded text-sm cursor-pointer " >High</h3>
+                    <h4 className="text-sm" >20  May 2025</h4>
+                </div>
+                <h2 className="mt-5 text-2xl font-bold " >Create a cup of tea and have in the morning</h2>
+                <p className="text-sm mt-3 " >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex aperiam aliquam quam optio quibusdam architecto.</p>
+            </div>
+
+            <div className=" h-full p-5 w-[300px] flex-shrink-0 bg-red-400 rounded-xl   " >   
+                <div className="flex justify-between items-center" >
+                    <h3 className="bg-red-600 px-3 py-1 rounded text-sm cursor-pointer " >High</h3>
+                    <h4 className="text-sm" >20  May 2025</h4>
+                </div>
+                <h2 className="mt-5 text-2xl font-bold " >Create a cup of tea and have in the morning</h2>
+                <p className="text-sm mt-3 " >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex aperiam aliquam quam optio quibusdam architecto.</p>
+            </div>
+
+            <div className=" h-full p-5 w-[300px] flex-shrink-0 bg-red-400 rounded-xl   " >   
+                <div className="flex justify-between items-center" >
+                    <h3 className="bg-red-600 px-3 py-1 rounded text-sm cursor-pointer " >High</h3>
+                    <h4 className="text-sm" >20  May 2025</h4>
+                </div>
+                <h2 className="mt-5 text-2xl font-bold " >Create a cup of tea and have in the morning</h2>
+                <p className="text-sm mt-3 " >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex aperiam aliquam quam optio quibusdam architecto.</p>
+            </div>
+
+            <div className=" h-full p-5 w-[300px] flex-shrink-0 bg-red-400 rounded-xl   " >   
+                <div className="flex justify-between items-center" >
+                    <h3 className="bg-red-600 px-3 py-1 rounded text-sm cursor-pointer " >High</h3>
+                    <h4 className="text-sm" >20  May 2025</h4>
+                </div>
+                <h2 className="mt-5 text-2xl font-bold " >Create a cup of tea and have in the morning</h2>
+                <p className="text-sm mt-3 " >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex aperiam aliquam quam optio quibusdam architecto.</p>
+            </div>
+
+            <div className=" h-full p-5 w-[300px] flex-shrink-0 bg-red-400 rounded-xl   " >   
+                <div className="flex justify-between items-center" >
+                    <h3 className="bg-red-600 px-3 py-1 rounded text-sm cursor-pointer " >High</h3>
+                    <h4 className="text-sm" >20  May 2025</h4>
+                </div>
+                <h2 className="mt-5 text-2xl font-bold " >Create a cup of tea and have in the morning</h2>
+                <p className="text-sm mt-3 " >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex aperiam aliquam quam optio quibusdam architecto.</p>
+            </div>
+
+            <div className=" h-full p-5 w-[300px] flex-shrink-0 bg-red-400 rounded-xl   " >   
+                <div className="flex justify-between items-center" >
+                    <h3 className="bg-red-600 px-3 py-1 rounded text-sm cursor-pointer " >High</h3>
+                    <h4 className="text-sm" >20  May 2025</h4>
+                </div>
+                <h2 className="mt-5 text-2xl font-bold " >Create a cup of tea and have in the morning</h2>
+                <p className="text-sm mt-3 " >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex aperiam aliquam quam optio quibusdam architecto.</p>
+            </div>
+
+            {/* <div className="h-full w-[300px] flex-shrink-0 bg-yellow-400 rounded-xl  " >   
+            </div>
+
+            <div className="h-full w-[300px] flex-shrink-0 bg-yellow-400 rounded-xl  " >   
+            </div>
+
+            <div className="h-full w-[300px] flex-shrink-0 bg-yellow-400 rounded-xl  " >   
+            </div>
+
+            <div className="h-full w-[300px] flex-shrink-0 bg-yellow-400 rounded-xl  " >   
+            </div>
+
+            <div className="h-full w-[300px] flex-shrink-0 bg-yellow-400 rounded-xl  " >    
+            </div> */}
+           
+        </div>
+
+        
+    )
+}
+
+export default TaskList
